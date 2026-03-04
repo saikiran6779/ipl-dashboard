b9ab95a095f9f0ef65c2539d565e6e24e695cf45
Sai Kiran Madhavaram <saikiranmadhavaram@gmail.com>
2026-03-04 05:12:46 -0500

    jpa repositories fix and enable for

--- backend/src/main/java/com/ipl/dashboard/IplDashboardApplication.java
+++ backend/src/main/java/com/ipl/dashboard/IplDashboardApplication.java
@@ -2,8 +2,10 @@ package com.ipl.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class IplDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(IplDashboardApplication.class, args);
